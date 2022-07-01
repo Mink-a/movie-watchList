464546eb57d95ff1d43a825b6191392ee65d8448
Mink-a <rahulakhant@gmail.com>
2022-07-01 20:50:20 +0630

    update with live data

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# web-platform-gtlyak
+# movie watch-list
+
+**Scrimba solo API project**
+
+[View Live ⚡️](https://web-platform-gtlyak.stackblitz.io/)
+
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gtlyak)
--- script.js
+++ script.js
@@ -9,44 +9,45 @@ function render() {
     .then((res) => res.json())
     .then((data) => {
       cardArr = data.Search;
-      
 
-      cardArr.forEach((item) => {
-        cardContainer.innerHTML += `
-    <!-- Card 
-    ---------------------------->
-    <div class="card">
-      <span class="hide">${item.imdbID}keyID</span>
-      <div class="img-container">
-        <img
-          src=${item.Poster}
-          alt="an image"
-          class="image"
-        />
-      </div>
-      <div class="info">
-        <h3 class="title">${item.Title}</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          cum nisi dicta!
-        </p>
-        <button class="add">Add</button>
-      </div>
-    </div>
-    <!---------------------------- 
-      Card End -->
-    `;
-      });
-      // localStorage.setItem('list', JSON.stringify(cardHtml));
-      // const list = JSON.parse(localStorage.getItem('list'));
-      // cardContainer.innerHTML = list.join('');
-      document.querySelectorAll('.add').forEach((item) => {
-        item.addEventListener('click', (e) => {
-          let movie = e.target.parentNode.parentNode;
-          let movieID = movie.childNodes[1].textContent;
-          // console.log(movie, movieID);
-          localStorage.setItem(movieID, movie.innerHTML);
-        });
+      const movieIDArr = cardArr.map((item) => item.imdbID);
+      movieIDArr.forEach((movieID) => {
+        fetch(`https://omdbapi.com/?apikey=6a00f621&i=${movieID}`)
+          .then((res) => res.json())
+          .then((data) => {
+            cardContainer.innerHTML += `
+              <!-- Card 
+              ---------------------------->
+              <div class="card">
+                <span class="hide">${data.imdbID}keyID</span>
+                <div class="img-container">
+                  <img
+                    src=${data.Poster}
+                    alt="an image"
+                    class="image"
+                  />
+                </div>
+                <div class="info">
+                  <h3 class="title">${data.Title}  <span class="rating">⭐ ${data.imdbRating}</span></h3>
+                  <p>
+                    ${data.Plot}
+                  </p>
+                  <button class="add">Add</button>
+                </div>
+              </div>
+              <!---------------------------- 
+                Card End -->
+              `;
+
+            document.querySelectorAll('.add').forEach((item) => {
+              item.addEventListener('click', (e) => {
+                let movie = e.target.parentNode.parentNode;
+                let movieID = movie.childNodes[1].textContent;
+                console.log(movie, movieID);
+                localStorage.setItem(movieID, movie.innerHTML);
+              });
+            });
+          });
       });
     });
 
